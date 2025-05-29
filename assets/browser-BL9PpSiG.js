1e496f75264203c1d23899e4c1190f9bcef08f3e
Parth Yadav <parthcompiler@gmail.com>
2025-05-29 18:41:41 +0530

    Updates

---
+++ assets/browser-BL9PpSiG.js
@@ -0,0 +1,2 @@
+import{g as e}from"./index-BHpXUj6A.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser-BL9PpSiG.js.map
